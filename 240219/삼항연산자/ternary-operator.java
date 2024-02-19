da662a5d62e534349f5342ee3abdac9b40f747fe
min5421 <114565439+min5421@users.noreply.github.com>
2024-02-19 19:34:10 +0900

    Title: 삼항연산자, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240219/삼항연산자/ternary-operator.java
+++ 240219/삼항연산자/ternary-operator.java
@@ -5,7 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        a = a != "100" ? "pass" : "failure";
+        a = a == "100" ? "pass" : "failure";
         System.out.println(a);
     }
 }
