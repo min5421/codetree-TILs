7ab467325d08bbced25e6ce42aaf6a0e9c9451e6
min5421 <114565439+min5421@users.noreply.github.com>
2024-02-22 19:28:09 +0900

    Title: 점수 비교, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240222/점수 비교/score-comparison.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int am = sc.nextInt();
+        int ae = sc.nextInt();
+        int bm = sc.nextInt();
+        int be = sc.nextInt();
+
+        if(am > bm && ae > be){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+}
