d77e62c7c358ed9ec51c610816ac59e938124362
min5421 <114565439+min5421@users.noreply.github.com>
2024-02-27 18:39:45 +0900

    Title: 좀 더 어려운 수학 점수, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240227/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int am = sc.nextInt();
+        int ae = sc.nextInt();
+        int bm = sc.nextInt();
+        int be = sc.nextInt();
+
+        if (am > bm) {
+            System.out.println("A");
+        }
+        else if(am < bm){
+            System.out.println("B");
+        }
+        else if(ae > be){
+            System.out.println("A");
+        }
+        else {
+            System.out.println("B");
+        }
+    }
+}
