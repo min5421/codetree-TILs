8630528346e630fdbf858284680c8f7b50fcf572
min5421 <114565439+min5421@users.noreply.github.com>
2024-02-29 20:36:22 +0900

    Title: a/b 출력, Time: 141ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240229/a/b 출력/a-divide-b.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double result = a / b;
+
+        System.out.printf("%.20f", result);
+        
+    }
+}
