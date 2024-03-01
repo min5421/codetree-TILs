49db1f8f4547ee77e60d0b0b03894dff1e116893
min5421 <114565439+min5421@users.noreply.github.com>
2024-03-01 23:14:20 +0900

    Title: 369 게임, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240301/369 게임/369-game.java
+++ 240301/369 게임/369-game.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            if (i % 3 == 0 || (i % 10) % 3 == 0 || (i > 10 && (i / 10) % 3 == 0)) {
                 System.out.print("0 ");
             }
             else {
