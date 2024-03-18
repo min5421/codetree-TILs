6e98f28af8e656fab13d09f2e09d23200bdb6af3
min5421 <114565439+min5421@users.noreply.github.com>
2024-03-18 21:55:36 +0900

    Title: 별표 출력하기 12, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 240318/별표 출력하기 12/print-star-12.java
+++ 240318/별표 출력하기 12/print-star-12.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i = 0; i < n/2*2; i++){
+        for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(i == 0 || (j >= i && j % 2 == 1)){
                     System.out.print("* ");
