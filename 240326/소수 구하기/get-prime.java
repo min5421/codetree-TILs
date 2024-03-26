a4081441e780e82a094b1c6038bb9f657ef32c15
min5421 <114565439+min5421@users.noreply.github.com>
2024-03-26 11:02:10 +0900

    Title: 소수 구하기, Time: 170ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240326/소수 구하기/get-prime.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 2; i <= n; i++){
+            int cnt = 0;
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
+                    cnt++;
+                }
+            }
+            if(cnt == 0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
