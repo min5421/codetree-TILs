71f72dcf846c81c30af7f17094ca47aba0b1eab5
min5421 <114565439+min5421@users.noreply.github.com>
2024-04-06 16:59:46 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240406/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n+2];
+
+        for(int i = 1; i <= n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 2; i <= n; i++){
+            for(int j = i; j > 1; j--){
+                if(arr[j] > arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
+
+        for(int i = 1; i <= n; i++){
+            if(arr[i] != arr[i-1] && arr[i] != arr[i+1]){
+                System.out.println(arr[i]);
+                break;
+            }
+        }
+    }
+}
