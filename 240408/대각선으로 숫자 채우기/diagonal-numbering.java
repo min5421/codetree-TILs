bdd3e362f67ad853b09a49a01b59e93c19001dd5
min5421 <114565439+min5421@users.noreply.github.com>
2024-04-08 09:14:15 +0900

    Title: 대각선으로 숫자 채우기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240408/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr = new int[n][m];
+
+        int num = 1;
+        if(m >= n){
+            for(int j = 0; j < n-1; j++){
+                for(int i = 0; i <= j; i++){
+                    arr[i][j-i] = num;
+                    num++;
+                }
+            }
+            for(int j = n-1; j < m; j++){
+                for(int i = 0; i < n; i++){
+                    arr[i][j-i] = num;
+                    num++;
+                }
+            }
+            for(int i = 1; i < n; i++){
+                for(int j = 0; j < n-i; j++){
+                    arr[i+j][m-j-1] = num;
+                    num++;
+                }
+            }
+        }
+        else{
+            for(int j = 0; j < m-1; j++){
+                for(int i = 0; i <= j; i++){
+                    arr[i][j-i] = num;
+                    num++;
+                }
+            }
+            for(int i = 0; i < m-1; i++){
+                for(int j = m-1; j >= 0; j--){
+                    arr[i+m-j-1][j] = num;
+                    num++;
+                }
+            }
+            for(int i = 1; i < m; i++){
+                for(int j = 0; j < m-i; j++){
+                    arr[m-2+i+j][m-j-1] = num;
+                    num++;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
