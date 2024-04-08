a11815556871a90da8f0ac7137fa21d567e050fb
min5421 <114565439+min5421@users.noreply.github.com>
2024-04-09 08:12:19 +0900

    Title: 파스칼의 삼각형, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240409/파스칼의 삼각형/pascal's-triangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] arr2d = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            arr2d[i][0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                arr2d[i][j] = arr2d[i - 1][j - 1] + arr2d[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(arr2d[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
