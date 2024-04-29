cc765a0f4803d78c489d85fbac10739ef437cb48
min5421 <114565439+min5421@users.noreply.github.com>
2024-04-29 14:33:11 +0900

    Title: 문자열 계속 지우기, Time: 3049ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

---
+++ 240429/문자열 계속 지우기/keep-removing-string.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        boolean check = true;
+        while(check){
+            boolean isThere = false;
+            for(int i = 0; i <= a.length()-b.length(); i++){
+                for(int j = 0; j < b.length(); j++){
+                    if(a.charAt(i+j) != b.charAt(j)){
+                        isThere = false;
+                        break;
+                    }
+                    else{
+                        isThere = true;
+                    }
+                }
+
+                if(isThere){
+                    a = a.substring(0,i)+a.substring(i+b.length());
+                    break;
+                }
+
+                if(i == a.length()-b.length()){
+                    check = false;
+                }
+            }
+        }
+        System.out.println(a);
+    }
+}
