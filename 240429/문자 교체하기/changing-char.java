95286c6c12dd19a390c7e8fb4d1c95fd124aa2cc
min5421 <114565439+min5421@users.noreply.github.com>
2024-04-29 13:18:58 +0900

    Title: 문자 교체하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240429/문자 교체하기/changing-char.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String t = sc.next();
+        System.out.println(s.substring(0,2)+t.substring(2));
+    }
+}
