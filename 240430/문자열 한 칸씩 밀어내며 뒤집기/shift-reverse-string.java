79836e4e2334e7d4575d50a20b231a33f90b668e
min5421 <114565439+min5421@users.noreply.github.com>
2024-04-30 13:48:48 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 697ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240430/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 240430/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -11,17 +11,18 @@ public class Main {
             int n = sc.nextInt();
             
             if(n == 1){
-                System.out.println(s = s.substring(1)+s.substring(0,1));
+                s = s.substring(1)+s.substring(0,1);
             }
             else if(n == 2){
-                System.out.println(s = s.substring(len-1,len)+s.substring(0,len-1));
+                s = s.substring(len-1,len)+s.substring(0,len-1);
             }
             else{
-                for(int j = len-1; j >= 0; j--){
-                    System.out.print(s.charAt(j));
+                for(int j = 0; j < len; j++){
+                    s = s.substring(0,j)+s.substring(len-1,len)+s.substring(j,len-1);
                 }
-                System.out.println();
             }
+
+            System.out.println(s);
         }
     }
 }
