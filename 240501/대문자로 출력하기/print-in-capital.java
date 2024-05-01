061bff1e0504e4c3d304fd978cb0dd265bc9f7b7
min5421 <114565439+min5421@users.noreply.github.com>
2024-05-01 18:24:59 +0900

    Title: 대문자로 출력하기, Time: 170ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240501/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c >= 'A'&& c <= 'Z'){
+                System.out.print(c);
+            }
+            else if(c >= 'a'&& c <= 'z'){
+                System.out.print((char)(c-'a'+'A'));
+            }
+        }
+    }
+}
