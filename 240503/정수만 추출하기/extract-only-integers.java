fd3b951c58694d914faaf7ba8dcded47dc741b93
min5421 <114565439+min5421@users.noreply.github.com>
2024-05-03 18:24:24 +0900

    Title: 정수만 추출하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240503/정수만 추출하기/extract-only-integers.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        int num1 = 0;
+        int num2 = 0;
+
+
+        for(int i = 0; i < a.length(); i++){
+            char c = a.charAt(i);
+            if(c >= '0' && c <= '9'){
+                num1 *= 10;
+                num1 += (int)(c-'0');
+            }
+            else{
+                break;
+            }
+        }
+        for(int i = 0; i < b.length(); i++){
+            char c = b.charAt(i);
+            if(c >= '0' && c <= '9'){
+                num2 *= 10;
+                num2 += (int)(c-'0');
+            }
+            else{
+                break;
+            }
+        }
+
+
+        System.out.println(num1+num2);
+    }
+}
