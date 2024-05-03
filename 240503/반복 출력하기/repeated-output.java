67d86d36439c2f44cfe4202a2f666a2f113e2a21
min5421 <114565439+min5421@users.noreply.github.com>
2024-05-03 21:25:27 +0900

    Title: 반복 출력하기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240503/반복 출력하기/repeated-output.java
+++ 240503/반복 출력하기/repeated-output.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        print(4);
+        print(n);
     }
 
     public static void print(int n){
