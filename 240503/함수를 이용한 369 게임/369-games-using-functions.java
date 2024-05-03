fe909f4bd15a1b5451464870b42cdeafdce02cc5
min5421 <114565439+min5421@users.noreply.github.com>
2024-05-03 23:05:46 +0900

    Title: 함수를 이용한 369 게임, Time: 212ms, Memory: 20MB, Status: Wrong Answer - CodeTree

---
+++ 240503/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int cnt = 0;
+
+        for(int i = a; i <= b; i++){
+            if(number(i) || triple(i)){
+                cnt++;
+            }
+        }
+        
+        System.out.println(cnt);
+    }
+
+    public static boolean number(int i){
+        String s = Integer.toString(i);
+        for(int j = 0; j < s.length(); j++){
+            if(((int)s.charAt(j)-'0') % 3 == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean triple(int i){
+        return i % 3 == 0;
+    }
+}
