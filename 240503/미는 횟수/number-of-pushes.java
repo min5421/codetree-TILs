5c56c47054714c2c1ee254754f6756f2a126c195
min5421 <114565439+min5421@users.noreply.github.com>
2024-05-03 20:33:32 +0900

    Title: 미는 횟수, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240503/미는 횟수/number-of-pushes.java
+++ 240503/미는 횟수/number-of-pushes.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
         String t = sc.next();
+        String s = sc.next();
         int n = 0;
         boolean equal = false;
 
-        for(int i = 0; i <= t.length(); i++){
+        for(int i = 0; i < t.length(); i++){
+            t = t.substring(t.length()-1) + t.substring(0,t.length()-1);
+            n++;
             if(s.equals(t)){
                 equal = true;
                 break;
             }
-            t = t.substring(t.length()-1) + t.substring(0,t.length()-1);
-            n++;
         }
 
-        if(!equal || s.length() > 100 || s.length() < 2){
+        if(!equal){
             n = -1;
         }
         System.out.println(n);
