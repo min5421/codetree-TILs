ae5a2a362214a9dbf07afcc61ac44cdb07643831
min5421 <114565439+min5421@users.noreply.github.com>
2024-05-05 21:38:52 +0900

    Title: 2021년 날짜의 유무, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240505/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        if(isit(m, n)){
+            System.out.println("Yes");
+        }    
+        else{
+            System.out.println("No");
+        }
+    }
+
+    public static boolean isit(int m, int n){
+        if(m == 2){
+            if(n <= 28){
+                return true;
+            }
+        }
+        else if(m == 4 || m == 6 || m == 9 || m == 10){
+            if(n <= 30){
+                return true;
+            }
+        }
+        else{
+            if(n <= 31){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
