9e17dbadea8ce53d80cebe425f67b4eccb963ddd
min5421 <114565439+min5421@users.noreply.github.com>
2024-05-06 09:16:12 +0900

    Title: palindrome 여부 판단하기, Time: 156ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240506/palindrome 여부 판단하기/determine-whether-palindrome-is-present.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        if(isPalindrome(s)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+
+    public static boolean isPalindrome(String s){
+        String t = "";
+
+        for(int i = s.length()-1; i >= 0; i--){
+            t += s.charAt(i);
+        }
+
+        if(s.equals(t)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
