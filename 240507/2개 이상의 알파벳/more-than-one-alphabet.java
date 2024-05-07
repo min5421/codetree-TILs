4c8848a0525da8d8a7956a71a79ea9a999ce409d
min5421 <114565439+min5421@users.noreply.github.com>
2024-05-07 13:36:08 +0900

    Title: 2개 이상의 알파벳, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240507/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        if(twoMore(a)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+
+    public static boolean twoMore(String a){
+        for(int i = 0; i < a.length()-1; i++){
+            for(int j = i+1; j < a.length(); j++){
+                if(i != j){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
