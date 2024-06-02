d990632963e716e783300c83261c747ace2fb421
min5421 <114565439+min5421@users.noreply.github.com>
2024-06-03 08:18:22 +0900

    Title: 중앙값 계산 2, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240603/중앙값 계산 2/get-median-2.java
+++ 240603/중앙값 계산 2/get-median-2.java
@@ -6,11 +6,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-
+        
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
             if(i % 2 == 0){
-                Arrays.sort(arr, 0, i);
+                Arrays.sort(arr, 0, i+1);
                 System.out.print(arr[i/2]+" ");
             }
         }
