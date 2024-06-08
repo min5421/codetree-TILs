6b3ef9c2e1eb5ec3b1a75e098f626bc250ec2339
min5421 <114565439+min5421@users.noreply.github.com>
2024-06-08 22:05:09 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240608/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
+++ 240608/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.java
@@ -18,7 +18,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Student[] students = new Student[5];
+        Student[] students = new Student[n];
 
         for(int i = 0; i < n; i++){
             String name = sc.next();
