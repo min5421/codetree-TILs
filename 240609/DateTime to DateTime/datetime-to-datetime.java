c244c22f1927c5634569daae17c73293f7fe58ac
min5421 <114565439+min5421@users.noreply.github.com>
2024-06-09 18:40:30 +0900

    Title: DateTime to DateTime, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240609/DateTime to DateTime/datetime-to-datetime.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int d = 11;
+        int h = 11;
+        int m = 11;
+        int time = 0;
+
+        if(check(a, b, c)){
+            while(!(d == a && h == b && m == c)){
+                time++;
+                m++;
+
+                if(m == 60){
+                    h++;
+                    m = 0;
+                }
+                if(h == 24){
+                    d++;
+                    h = 0;
+                }
+            }
+            System.out.println(time);
+        }
+        else{
+            System.out.println(-1);
+        }
+    }
+
+    public static boolean check(int a, int b, int c){
+        if(a < 11){
+            return false;
+        }
+        else if(b < 11){
+            return false;
+        }
+        else if(c < 11){
+            return false;
+        }
+        return true;
+    }
+}
