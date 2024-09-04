677ccd63ec34b9bcb820cb88460832ef6f80e5e0
min5421 <114565439+min5421@users.noreply.github.com>
2024-09-05 07:11:18 +0900

    Title: 십진수와 이진수 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240905/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 240905/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -7,15 +7,29 @@ public class Main {
         
         n = toTen(n);
         n *= 17;
-        n = toTwo(n);
+        
+        int[] arr = new int[32];
+        int cnt = 0;
+        while(true){
+            if(n < 2){
+                arr[cnt] = n;
+                break;
+            }
 
-        System.out.println(n);
+            arr[cnt] = n % 2;
+            n /= 2;
+            cnt++;
+        }
+
+        for(int i = cnt; i >= 0; i--){
+            System.out.print(arr[i]);
+        }
         
     }
 
     static int toTen(int n){
         int num = 0;
-        int[] arr = new int[20];
+        int[] arr = new int[32];
 
         int cnt = 0;
         while(n > 0){
@@ -26,30 +40,6 @@ public class Main {
 
         for(int i = cnt-1; i >= 0; i--){
             num *= 2;
-            num += arr[i];
-        }
-
-        return num;
-    }
-
-    static int toTwo(int n){
-        int num = 0;
-        int[] arr = new int[20];
-
-        int cnt = 0;
-        while(true){
-            if(n < 2){
-                arr[cnt] = n;
-                break;
-            }
-
-            arr[cnt] = n % 2;
-            n = n / 2;
-            cnt++;
-        }
-
-        for(int i = cnt; i >= 0; i--){
-            num *= 10;
             num += arr[i];
         }
 
