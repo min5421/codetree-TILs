0bfa0fcad4f4bd1662d6ebc7bc0472f49b7efdae
조민서 <114565439+min5421@users.noreply.github.com>
2025-02-01 19:57:31 +0900

    Title: 1부터 n까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250201/1부터 n까지 출력/print-from-1-to-n.c
+++ 250201/1부터 n까지 출력/print-from-1-to-n.c
@@ -4,7 +4,8 @@ int main() {
     // Please write your code here.
     int n;
     scanf("%d", &n);
-    for(int a=n; a<=n; n++);
-    printf("%d", a);
+    for(int a=1; a<=n; a++){
+        printf("%d ", a);
+    }
     return 0;
 }
