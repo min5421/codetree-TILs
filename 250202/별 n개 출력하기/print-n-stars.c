dfeb066c63445e2f651200ca320c43b560d03a02
조민서 <114565439+min5421@users.noreply.github.com>
2025-02-02 22:22:44 +0900

    Title: 별 n개 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250202/별 n개 출력하기/print-n-stars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    int i = 1;
+    scanf("%d", &n);
+    while(i <= n){
+        printf("*\n");
+        i++;
+    }
+    return 0;
+}
