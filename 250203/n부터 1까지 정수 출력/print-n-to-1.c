48413b290333424b1f4cea4e1bed5de21fa42924
조민서 <114565439+min5421@users.noreply.github.com>
2025-02-03 20:47:31 +0900

    Title: n부터 1까지 정수 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/n부터 1까지 정수 출력/print-n-to-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+    int i=n;
+    while(i>=1){
+        printf("%d ", i);
+        i--;
+    }
+    return 0;
+}
