7651884aa7aa82dbfa3290e048d5cdaaccf6f1d4
조민서 <114565439+min5421@users.noreply.github.com>
2025-02-03 21:44:08 +0900

    Title: n번 더하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250203/n번 더하기/add-n-times.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, n;
+    scanf("%d %d", &a, &n);
+    for(int i=1; i<=n; i++){
+        printf("%d\n", a+n*i);
+    }
+    return 0;
+}
