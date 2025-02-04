ea335cd6ae96974ee8007f336850f8750bf17bc1
조민서 <114565439+min5421@users.noreply.github.com>
2025-02-04 17:22:45 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250204/369 게임/369-game.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++){
+        if(i%3==0){
+            printf("0 ");
+        }
+        else if(i%10==3||i%10==6||i%10==9){
+            printf("0 ");
+        }
+        else if(i/10==3||i/10==6||i/10==9){
+            printf("0 ");
+        }
+        else{
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
