b24dc28efa7ac315fceb4932dda5b4955919bba6
조민서 <114565439+min5421@users.noreply.github.com>
2025-02-06 14:43:05 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250206/a의 b승/a-to-the-power-of-b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    scanf("%d %d", &a, &b);
+    int prod=1;
+    for(int i=1; i<=b; i++){
+        prod*=a;
+    }
+    printf("%d", prod);
+    return 0;
+}
