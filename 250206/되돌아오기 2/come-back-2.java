13037fb776b2a79cb4eb5d207ee131c17509d1ec
조민서 <114565439+min5421@users.noreply.github.com>
2025-02-06 22:31:47 +0900

    Title: 되돌아오기 2, Time: 197ms, Memory: 11MB, Status: Runtime Error - CodeTree

---
+++ 250206/되돌아오기 2/come-back-2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int n = s.length();
+        int[] dx = {0, -1, 0, 1};
+        int[] dy = {1, 0, -1, 0};
+        int x = 0;
+        int y = 0;
+        int dirNum = 3;
+
+        int cnt = 0;
+        for(int i = 0;i < n; i++){
+            if(s.charAt(i) == 'L'){
+                dirNum = (dirNum - 1) % 4;
+            }
+            else if(s.charAt(i) == 'R'){
+                dirNum = (dirNum + 1) % 4;
+            }
+            else {
+                x += dx[dirNum];
+                y += dy[dirNum];
+            }
+            cnt++;
+
+            if(x == 0 && y == 0){
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
