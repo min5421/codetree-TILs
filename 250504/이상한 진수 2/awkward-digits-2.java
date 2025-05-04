cdf5f5ff474973739ecca499e58d8522fd875010
조민서 <114565439+min5421@users.noreply.github.com>
2025-05-04 19:12:05 +0900

    Title: 이상한 진수 2, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250504/이상한 진수 2/awkward-digits-2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+
+    static String changeNum(String a, int i){
+        if(a.charAt(i) == 1){
+            return a.substring(0, i) + "0"  + a.substring(i+1);
+        }
+        else{
+            return a.substring(0, i) + "1"  + a.substring(i+1);
+        }
+    }
+
+    static int twoToTen(String num){
+        int result = 0;
+        for(int i = 0; i < num.length(); i++){
+            result += (int)(num.charAt(num.length()-i-1)-48) * (int)Math.pow(2, i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i < a.length(); i++){
+            String num = changeNum(a, i);
+            //System.out.println(num+" = "+twoToTen(num));
+            max = Math.max(twoToTen(num), max);
+        }
+
+        System.out.println(max);
+    }
+
+    
+}
